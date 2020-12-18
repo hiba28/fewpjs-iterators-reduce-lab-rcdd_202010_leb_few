aff355a1d34fac1fe753195387a4cd77e65fbe1f
Hiba Abdel karim <hibaabdelkarim2000@gmail.com>
2020-12-18 04:45:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-function sumup() {
-  let sum = products.reduce((a,b) => a+b)
-  return sum
-}
+let totalBatteries = batteryBatches.reduce((acc,curr) => acc+curr)
